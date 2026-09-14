a7476169e06009e2688e9ee08c2d8537052fb27e
Sardor <ramanovsardor8@gmail.com>
2026-09-14 10:49:26 +0300

    feat: add Android safe-area manager

---
+++ src/safe_area_manager.gd
@@ -0,0 +1,36 @@
+class_name SafeAreaManager
+extends Node
+
+const MIN_MARGIN := 20
+const CONTENT_GAP := 16
+
+func _ready() -> void:
+    get_viewport().size_changed.connect(_refresh)
+    get_tree().node_added.connect(_on_node_added)
+    call_deferred("_refresh")
+
+func _on_node_added(node: Node) -> void:
+    if node is Control:
+        call_deferred("_refresh")
+
+func _refresh() -> void:
+    var shell := get_tree().current_scene
+    if shell == null:
+        return
+    var target := shell.find_child("SafeAreaShell", true, false)
+    if target is MarginContainer:
+        _apply_margins(target)
+
+func _apply_margins(container: MarginContainer) -> void:
+    var viewport_size := get_viewport().get_visible_rect().size
+    if viewport_size.x <= 0.0 or viewport_size.y <= 0.0:
+        return
+    var safe := DisplayServer.get_display_safe_area()
+    var left := clampi(int(safe.position.x), 0, int(viewport_size.x))
+    var top := clampi(int(safe.position.y), 0, int(viewport_size.y))
+    var right := clampi(int(viewport_size.x - safe.end.x), 0, int(viewport_size.x))
+    var bottom := clampi(int(viewport_size.y - safe.end.y), 0, int(viewport_size.y))
+    container.add_theme_constant_override("margin_left", maxi(MIN_MARGIN, left + CONTENT_GAP))
+    container.add_theme_constant_override("margin_top", maxi(MIN_MARGIN, top + CONTENT_GAP))
+    container.add_theme_constant_override("margin_right", maxi(MIN_MARGIN, right + CONTENT_GAP))
+    container.add_theme_constant_override("margin_bottom", maxi(MIN_MARGIN, bottom + CONTENT_GAP))
